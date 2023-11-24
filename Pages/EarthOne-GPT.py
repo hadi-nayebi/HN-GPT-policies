3120884693aaaeb64671987848395aae1ebe6755
hadinayebi <hadinayebi@gmail.com>
2023-11-24 15:31:27 -0800

    Update privacy policy for EarthOne's AI assistant

--- Pages/EarthOne-GPT.py
+++ Pages/EarthOne-GPT.py
@@ -2,10 +2,34 @@ import streamlit as st
 
 def main():
     st.title("Privacy Policy")
-    st.write("This is the privacy policy for our website.")
-    st.write("We value your privacy and are committed to protecting your personal information.")
-    st.write("Please read this privacy policy carefully to understand how we collect, use, and disclose your personal information.")
-    st.write("If you have any questions or concerns about our privacy policy, please contact us.")
+    st.write("""As EarthOne's AI assistant, I am designed to provide strategic, technical, and operational support specifically for the EarthOne biotech startup. Here are the user policies for interacting with me, tailored to the founding team's needs:
+
+User Policies for EarthOne's AI Assistant (EarthOne)
+1. Access and Use Restrictions: 
+Eligibility: Only EarthOne's founding team members, namely Hadi and Jonathan, are authorized to interact with me.
+Purpose of Use: My primary role is to assist in developing and managing EarthOne’s operations, strategies, and technologies. Usage outside these areas is not supported.
+2. Confidentiality and Data Handling:
+Data Security: Confidentiality is paramount. All interactions, including shared documents and discussions, are strictly confidential and to be used solely for EarthOne's development.
+Information Storage: Information shared with me will be stored securely and will not be shared with external parties.
+3. Interaction Guidelines:
+Single-Question Policy: To maintain clarity and efficiency, please provide one question or instruction per interaction.
+Clarity of Requests: Provide clear, concise, and specific requests or questions to ensure accurate and relevant responses.
+Feedback Loop: Regular feedback on my responses is encouraged to refine and improve future assistance.
+4. Scope of Assistance:
+Technical Support: I am equipped to assist with biotech-specific inquiries, including microfluidic device design, strain development, and biotech R&D strategies.
+Business Strategy: I can provide insights into strategic planning, market analysis, and funding strategies specific to biotech startups.
+Operational Management: Assistance in project management, operational workflows, and task prioritization is available.
+5. Limitations:
+Ethical Boundaries: Advice and solutions must adhere to ethical standards in biotechnology and business. I will not support requests that conflict with ethical norms or legal regulations.
+Complexity of Tasks: While I am designed to handle a range of complex tasks, some highly specialized or novel challenges might be beyond my current capabilities.
+6. Continuous Learning:
+Adaptation: My responses and capabilities will evolve over time, reflecting the ongoing development in biotech, AI, and EarthOne's specific needs.
+Updates: Regular updates on EarthOne’s progress, challenges, and shifts in strategy are crucial for providing relevant and up-to-date assistance.
+7. User Autonomy:
+Decision-Making: While I provide options and suggestions, final decisions rest with the founding team. I respect user autonomy in decision-making.
+8. Compliance with Legal Standards:
+Regulatory Adherence: All advice and solutions will comply with the applicable legal and regulatory standards in the biotech industry.
+By adhering to these policies, the founding team of EarthOne can effectively utilize my capabilities to advance the startup's objectives.""")
 
 if __name__ == "__main__":
     main()
